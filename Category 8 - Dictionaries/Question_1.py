9f9a32d6c9d7e96db8fc006106f8d012a333adad
Lia Pires <102235852+Lia-Pires@users.noreply.github.com>
2022-06-09 12:07:03 -0300

    Adding questions
    Adding solution to questions 1 to 3, Category 8 - Dictionaries

---
+++ Category 8 - Dictionaries/Question_1.py
@@ -0,0 +1,21 @@
+# Convert the below lists into a dictionary
+
+keys = ['Ten', 'Twenty', 'Thirty']
+values = [10, 20, 30]
+
+dictionary = dict(zip(keys, values))
+
+print(dictionary)
+
+'''
+Muslim solution:
+
+keys = ['Ten', 'Twenty', 'Thirty']
+values = [10, 20, 30]
+
+result_dict = dict(zip(keys, values))
+
+print(result_dict)
+
+'''
+
