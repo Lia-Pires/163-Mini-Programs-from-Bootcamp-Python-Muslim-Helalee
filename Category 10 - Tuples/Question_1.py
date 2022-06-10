030ceecda2195d010ae1b9ff97d9eb9aa34f0f60
Lia Pires <102235852+Lia-Pires@users.noreply.github.com>
2022-06-10 12:07:46 -0300

    Adding questions:

---
+++ Category 10 - Tuples/Question_1.py
@@ -0,0 +1,15 @@
+# Reverse the following tuple
+
+tuple = (10, 20, 30, 40, 50)
+reversed_tuple = tuple[::-1]
+print(reversed_tuple)
+
+
+'''
+Muslim solution:
+tuple = (10, 20, 30, 40, 50)
+
+tuple = tuple[::-1]
+print(tuple)
+
+'''
---
+++ Category 10 - Tuples/Question_2.py
@@ -0,0 +1,14 @@
+# Access value 20 from the following tuple
+
+tuple = ("Orange", [10, 20, 30], (5, 15, 25))
+
+print(tuple[1][1])
+
+'''
+Muslim solution:
+tuple = ("Orange", [10, 20, 30], (5, 15, 25))
+
+print(tuple[1][1])
+
+
+'''
