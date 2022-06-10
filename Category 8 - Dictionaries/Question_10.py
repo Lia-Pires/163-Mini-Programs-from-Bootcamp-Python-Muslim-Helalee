6519328b6951ff89a212e3d577419f238b6d6024
Lia Pires <102235852+Lia-Pires@users.noreply.github.com>
2022-06-10 11:28:42 -0300

    Adding Questions
    Adding questions 9 and 10 from Category 8 - Dictionaries

---
+++ Category 8 - Dictionaries/Question_10.py
@@ -0,0 +1,27 @@
+# Change Emily’s salary to 8500 in the below dictionary
+
+
+sample_dict = {
+    'emp1': {'name': 'John', 'salary': 7500},
+    'emp2': {'name': 'Jane', 'salary': 8000},
+    'emp3': {'name': 'Emily', 'salary': 6500}
+}
+
+
+sample_dict["emp3"]["salary"] = 8500
+print(sample_dict)
+
+
+'''
+Muslim solution:
+
+sample_dict = {
+    'emp1': {'name': 'John', 'salary': 7500},
+    'emp2': {'name': 'Jane', 'salary': 8000},
+    'emp3': {'name': 'Emily', 'salary': 6500}
+}
+
+sample_dict["emp3"]['salary'] = 8500
+print(sample_dict["emp3"])
+
+'''
---
+++ Category 8 - Dictionaries/Question_9.py
@@ -0,0 +1,23 @@
+# Get the key of a minimum value from the following dictionary
+
+sample_dict = {
+    'Physics': 82,
+    'Math': 65,
+    'history': 75
+}
+
+
+print(min(sample_dict))
+
+
+'''
+Muslim solution:
+sample_dict = {
+    'Physics': 82,
+    'Math': 65,
+    'history': 75
+}
+
+print(min(sample_dict, key=sample_dict.get))
+
+'''
