1e8179227c4f1764931ce797d6fafc8983c16dad
Lia Pires <102235852+Lia-Pires@users.noreply.github.com>
2022-06-19 10:18:59 -0300

    Adding muslim solution
    Adding Muslim solution to my question

--- Category 12 - OOP/Question_1.py
+++ Category 12 - OOP/Question_1.py
@@ -8,3 +8,17 @@ class Vehicle:
 
 car = Vehicle(300, 25)
 print(car.max_speed, car.mileage)
+
+
+'''
+Muslim solution:
+class Vehicle:
+    def __init__(self, max_speed, mileage):
+        self.max_speed = max_speed
+        self.mileage = mileage
+
+
+some_model = Vehicle(220, 15)
+print(some_model.max_speed, some_model.mileage)
+
+'''
